4c614d3cc863e2d6db318e482781c1b3e61a27e7
jamesryan094 <james.ryan@my.jcu.edu.au>
2016-03-23 13:42:17 +1000

    Added function to error check user input

--- Week 3/ascii_codes.py
+++ Week 3/ascii_codes.py
@@ -1,3 +1,24 @@
-for i in range(1, 129):
-    print("Character: {}    code: {:>3}".format(chr(i), i))
-    i += 1
+def get_number(lower, upper):
+    is_valid = False
+    while not is_valid:
+        try:
+            num = int(input("Please enter a number between {} - {} (inclusive):".format(lower, upper)))
+            if num in range(lower, upper+1):
+                is_valid = True
+            else:
+                print("Please enter a valid number!")
+        except ValueError:
+            print("enter a numeral")
+    return num
+
+
+minimum = 1
+maximum = 136
+print(chr(get_number(minimum, maximum)))
+
+
+
+
+# for i in range(1, 129):
+#     print("Character: {}    code: {:>3}".format(chr(i), i))
+#     i += 1
